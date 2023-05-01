7058ca295bb73f56279d172ce6c81dd19fc6c23d
Gabriele Viganò <gabriviga2005@gmail.com>
2023-05-01 21:45:47 +0200

    Aggiungere i file di progetto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace EsercizioVerifica2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lbl = new System.Windows.Forms.Label();
+            this.txt = new System.Windows.Forms.TextBox();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 250;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lbl
+            // 
+            this.lbl.AutoSize = true;
+            this.lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl.Location = new System.Drawing.Point(372, 160);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(152, 55);
+            this.lbl.TabIndex = 0;
+            this.lbl.Text = "label1";
+            // 
+            // txt
+            // 
+            this.txt.Location = new System.Drawing.Point(138, 190);
+            this.txt.Name = "txt";
+            this.txt.Size = new System.Drawing.Size(100, 20);
+            this.txt.TabIndex = 1;
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl2.Location = new System.Drawing.Point(369, 242);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(204, 73);
+            this.lbl2.TabIndex = 2;
+            this.lbl2.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.txt);
+            this.Controls.Add(this.lbl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lbl;
+        private System.Windows.Forms.TextBox txt;
+        private System.Windows.Forms.Label lbl2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EsercizioVerifica2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int contTick = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(contTick == 10)
+            {
+                DateTime data = DateTime.Now;
+                lbl.Text = string.Format("{0}:{1}:{2}", data.Hour, data.Minute, data.Second);
+                contTick = 0;
+                int add;
+                if(int.TryParse(txt.Text, out add)) {
+                    data = data.AddHours(add);
+                }
+                lbl2.Text = string.Format("{0}:{1}:{2}", data.Hour, data.Minute, data.Second); 
+            }
+            contTick++;
+        }
+    }
+}
